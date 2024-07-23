67845bd0e8dbd14913f255ccfe5e1bb6e54242f7
ⱤɆ₱ØⱤ₮ <110277810+rep0rtDev@users.noreply.github.com>
2024-07-23 16:59:49 +0500

    Create filter.js

---
+++ src/text/filter.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+import fetch from 'node-fetch';
+
+class TextAPI {
+  static filter(api, data) {
+    const url = `${api.BASE_URL}/text/filter`;
+    return axios.post(url, data, { headers: api.headers })
+      .then(response => response.data);
+  }
+
+  static async asyncFilter(api, data) {
+    const url = `${api.BASE_URL}/text/gpt-new`;
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: api.headers,
+      body: JSON.stringify(data)
+    });
+    return await response.json();
+  }
+}
+
+export { TextAPI };
