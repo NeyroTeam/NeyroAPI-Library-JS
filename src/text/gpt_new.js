5b74d3c0571d0fe09bb3ccf1f6a56cab4ebd832e
ⱤɆ₱ØⱤ₮ <110277810+rep0rtDev@users.noreply.github.com>
2024-07-23 17:28:34 +0500

    Update gpt_new.js

--- src/text/gpt_new.js
+++ src/text/gpt_new.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import fetch from 'node-fetch';
 
-class TextAPI {
+class GptNew {
   static gptNew(api, data) {
     const url = `${api.BASE_URL}/text/gpt-new`;
     return axios.post(url, data, { headers: api.headers })
@@ -19,4 +19,4 @@ class TextAPI {
   }
 }
 
-export { TextAPI };
+export { GptNew };
